13792f0c7426273fa2e4ddb98a3b06bb69195c4a
JamesPotterr <161749116+JamesPotterr@users.noreply.github.com>
2024-08-17 08:42:00 +0200

    Update IAdaptiveCurveIrm.sol
    Signed-off-by: JamesPotterr <161749116+JamesPotterr@users.noreply.github.com>

--- src/adaptive-curve-irm/interfaces/IAdaptiveCurveIrm.sol
+++ src/adaptive-curve-irm/interfaces/IAdaptiveCurveIrm.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
+pragma solidity >=0.8.0;
 
 import {IIrm} from "../../../lib/morpho-blue/src/interfaces/IIrm.sol";
 import {Id} from "../../../lib/morpho-blue/src/interfaces/IMorpho.sol";
@@ -9,10 +9,12 @@ import {Id} from "../../../lib/morpho-blue/src/interfaces/IMorpho.sol";
 /// @custom:contact security@morpho.org
 /// @notice Interface exposed by the AdaptiveCurveIrm.
 interface IAdaptiveCurveIrm is IIrm {
-    /// @notice Address of Morpho.
+    /// @notice Returns the address of the Morpho contract.
     function MORPHO() external view returns (address);
 
-    /// @notice Rate at target utilization.
-    /// @dev Tells the height of the curve.
-    function rateAtTarget(Id id) external view returns (int256);
+    /// @notice Returns the rate at target utilization for a given market.
+    /// @dev Tells the height of the curve. The 'Id' type is used to reference a specific market.
+    /// @param id The identifier of the market.
+    /// @return rate The rate at the target utilization.
+    function rateAtTarget(Id id) external view returns (int256 rate);
 }
